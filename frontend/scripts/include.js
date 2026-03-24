51a7baf6c76d4f79e36cf43e4989d6b853070b47
GrntKim <grantjun0224@gmail.com>
2026-03-24 12:30:12 +0900

    refactor: separate header and footer components using fetch API

---
+++ frontend/scripts/include.js
@@ -0,0 +1,13 @@
+// Load header template
+fetch('components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    });
+    
+// Load footer template
+fetch('components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    });
