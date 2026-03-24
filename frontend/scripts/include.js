4a8f96099915881667be6af44d91c82deae31783
GrntKim <grantjun0224@gmail.com>
2026-03-24 13:05:10 +0900

    Solve fouc problem

--- frontend/scripts/include.js
+++ frontend/scripts/include.js
@@ -1,13 +1,15 @@
-// Load header template
-fetch('components/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    });
+Promise.all([
+    fetch('components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        }),
     
-// Load footer template
-fetch('components/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-    });
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+]).then(() => {
+    document.body.classList.remove('no-fouc');
+});
